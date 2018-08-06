b9af8597401323f33df4bfa2369a7add01de318a
Diego Cabrera <dcabrera@ups.edu.ec>
2018-08-06 16:39:00 -0500

    - Rutina de retardo, agregada. - Uso de APIs de apagado, agregado. - Parpadeo completado.

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include "Board_LED.h"
+#include <stdint.h>
+
+void delay(void)
+{
+	for(uint32_t i=0; i<500000; i++);
+}
 
 int main(void)
 {
-	LED_Initialize();
-	LED_On(0);
-	LED_On(1);
-	LED_On(2);
-	LED_On(3);
+	while(1)
+	{
+		LED_Initialize();
+		LED_On(0);
+		LED_On(1);
+		LED_On(2);
+		LED_On(3);
+		delay();
+		LED_Off(0);
+		LED_Off(1);
+		LED_Off(2);
+		LED_Off(3);
+		delay();
+	}
 	return 0;
 }
