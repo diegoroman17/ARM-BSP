6200a39d548820f6291f327499bccad7e9f1eb7b
Diego Cabrera <dcabrera@ups.edu.ec>
2018-08-06 19:56:54 -0500

    - Se agrega fun1, fun2 y fun3 con error de dirección en fun3 para mostrar la utilidad de call stack window

--- main.c
+++ main.c
@@ -6,10 +6,27 @@ void delay(void)
 	for(uint32_t i=0; i<500000; i++);
 }
 
+void fun3(void)
+{
+	void (*jump_addr) (void) = 0x00000000;
+	jump_addr();
+}
+
+void fun2(void)
+{
+	fun3();
+}
+
+void fun1(void)
+{
+	fun2();
+}
+
 int main(void)
 {
 	while(1)
 	{
+		fun1();
 		LED_Initialize();
 		LED_On(0);
 		LED_On(1);
